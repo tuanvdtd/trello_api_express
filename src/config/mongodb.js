d36d61eb3b39aa3ae1dd8248491fd77e724bb65f
tuanvdtd <chuhoi009.dt@gmail.com>
2025-05-10 12:23:03 +0700

    Refactor code structure for improved readability and maintainability

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,29 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable indent */
+const MOGODB_URI = 'mongodb+srv://tuanvdtd:qgjxQqJLUbKz5jvz@cluster0-tuandt.vg1kdkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-TuanDT'
+const DATABASE_NAME = 'express-api-database'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let dbInstance = null
+const mongoClient = new MongoClient(MOGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+export const DB_CONNECT = async () => {
+    await mongoClient.connect();
+    dbInstance = mongoClient.db(DATABASE_NAME)
+}
+
+export const DB_GET = () => {
+    if (!dbInstance) {
+        throw new Error('Database must be connected berfore using');
+    } else return dbInstance
+}
+
+export const DB_CLOSE = async () => {
+    await mongoClient.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,54 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-console */
+/* eslint-disable indent */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { DB_CONNECT, DB_GET, DB_CLOSE } from './config/mongodb'
+import exitHook from 'async-exit-hook'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
+  // process.stdin.resume()
+  const hostname = 'localhost'
+  const port = 8017
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await DB_GET().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-    { id: 'id-2', name: 'Two' },
-    { id: 'id-3', name: 'Three' },
-    { id: 'id-4', name: 'Four' },
-    { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Tuan, I am running at http://${hostname}:${port}/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${hostname}:${port}/`)
-})
+  exitHook(async () => {
+    console.log('Closing database connection...')
+    DB_CLOSE()
+    console.log('Database connection closed')
+  })
+}
+
+(async () => {
+  try {
+    await DB_CONNECT()
+    START_SERVER()
+  }
+  catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+})()
+
+// DB_CONNECT()
+//   .then(() => {
+//     console.log('Database connected successfully')
+//   })
+//   .then(() => {
+//     START_SERVER()
+//   })
+//   .catch(err => {
+//     // eslint-disable-next-line no-console
+//     console.error(err)
+//     process.exit(1)
+//   })
