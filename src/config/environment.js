1452d67afaae5ec8492dc3d96c654361eece4d8e
tuanvdtd <chuhoi009.dt@gmail.com>
2025-08-10 16:32:52 +0700

    Tạo resend để gửi link verification về mail cho người dùng xác thực

--- src/config/environment.js
+++ src/config/environment.js
@@ -6,5 +6,8 @@ export const env = {
   PORT: process.env.APP_PORT,
   HOSTNAME: process.env.APP_HOST,
   AUTHOR: process.env.AUTHOR,
-  BUILD_MODE: process.env.BUILD_MODE
+  BUILD_MODE: process.env.BUILD_MODE,
+  WEBSITE_DOMAIN_DEV: process.env.WEBSITE_DOMAIN_DEV,
+  WEBSITE_DOMAIN_PRODUCTION: process.env.WEBSITE_DOMAIN_PRODUCTION,
+  RESEND_API_KEY: process.env.RESEND_API_KEY
 }
